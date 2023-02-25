1df08feea50509368f981baa5db3b7aa6e2f0009
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-25 14:17:15 -0500

    modify movies index controller to include profile

--- controllers/movies.js
+++ controllers/movies.js
@@ -12,7 +12,9 @@ const create = async (req, res) => {
 
 const index = async (req, res) => {
   try {
-    const movies = await Movie.findAll()
+    const movies = await Movie.findAll({
+      include: 'profile'
+    })
     res.status(200).json(movies)
   } catch (error) {
     res.status(500).json(error)
