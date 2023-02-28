02e168f3238562875b16b6a1cf2040f4d0589319
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-02-28 11:49:32 -0500

    define new controller and route to get the logged-in user's profile

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -39,8 +39,18 @@ const show = async (req, res) => {
   }
 }
 
+const myProfile = async (req, res) => {
+  try {
+    const profile = await Profile.findByPk(req.user.profile.id)
+    res.status(200).json(profile)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
 module.exports = {
   index,
   addPhoto,
   show,
+  myProfile,
 }
